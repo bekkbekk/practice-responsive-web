1aaa12b516d7956326e8d8e7098eca10a6501bb7
Jerwin <jerwin.jek@gmail.com>
2022-09-02 21:18:13 +0800

    mobile nav

--- scripts/main.js
+++ scripts/main.js
@@ -8,4 +8,12 @@ window.onload = function() {
             document.querySelector("header").classList.remove('is-scrolling');
         }
     });
+
+    const menu_btn = document.querySelector('.hamburger');
+    const mobile_nav = document.querySelector('.mobile-nav');
+
+    menu_btn.addEventListener('click', function() {
+        menu_btn.classList.toggle('is-active');
+        mobile_nav.classList.toggle('is-active');
+    });
 }
